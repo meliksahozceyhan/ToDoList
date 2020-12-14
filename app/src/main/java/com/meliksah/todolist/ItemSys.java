bfeed559e5624dc8b4939eab3675ee29e23fa429
DMR <demirkosterit@gmail.com>
2020-12-14 15:06:28 +0300

    .

--- app/src/main/java/com/meliksah/todolist/ItemSys.java
+++ app/src/main/java/com/meliksah/todolist/ItemSys.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ItemSys {
-    private static ArrayList<Item> Items;
+    private static ArrayList<Item> items;
 
     public static Item addItem(int id, String name, String note){
         Item item = new Item(id,name,note);
-        Items.add(item);
+        items.add(item);
         return(item);
     }
+
+    public static void removeItem(int id){
+        for(Item i:items){
+            if(i.getId() == id)
+                items.remove(i);
+        }
+    }
     public static ArrayList<Item> getSocialItems() {
-        return Items;
+        return items;
     }
 }
